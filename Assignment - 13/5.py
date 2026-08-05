c88e5ecebcb95071c4b9ccee3a7bb21620f3703c
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-05 18:41:02 +0530

    Add files via upload

---
+++ Assignment - 13/5.py
@@ -0,0 +1,8 @@
+age_years = int(input("Enter your age in years : "))
+
+age_months = age_years * 12
+age_days = age_years * 365
+
+print("Your age in years : ", age_years)
+print("Your age in months : ", age_months)
+print("yours age in days : ", age_days)
