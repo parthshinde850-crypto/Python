a6f0bbcee7bcee5c887d6a97c87f62582262857a
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-10 18:33:26 +0530

    Add files via upload

---
+++ Advance Python Program/format.py
@@ -0,0 +1,8 @@
+a = "{} is a good boy {}".format("Parth" , "Boy")
+print(a)
+
+a = "{0} is a good boy {1}".format("Parth" , "Boy")
+print(a)
+
+a = "{1} is a good boy {0}".format("Parth" , "Boy")
+print(a)
