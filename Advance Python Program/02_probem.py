342f16ebad0a6be8376f608f875fb7c32a6f6f15
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-11 18:12:00 +0530

    Add files via upload

---
+++ Advance Python Program/02_probem.py
@@ -0,0 +1,6 @@
+name = input("Enter Name:")
+marks = int(input("Enter Marks:"))
+phone = int(input("Phoen Number:"))
+
+s = "The name of student {}, his marks are {} and phoen number is {}".format(name, marks, phone)
+print(s)
