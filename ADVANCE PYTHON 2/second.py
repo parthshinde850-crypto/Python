3c6251b25ccbdd31a3dabc6fc9ef1be266b3861e
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-22 17:57:29 +0530

    Add files via upload

---
+++ ADVANCE PYTHON 2/second.py
@@ -0,0 +1,21 @@
+class Student:
+    college = "MIT ADT University"   
+
+    def __init__(self, name, age, roll_no):
+        self.name = name
+        self.age = age
+        self.roll_no = roll_no
+
+    def display(self):
+        print("College :", Student.college)
+        print("Name :", self.name)
+        print("Age :", self.age)
+        print("Roll No :", self.roll_no)
+
+
+student1 = Student("Parth", 20, 33)
+Student2 = Student("aditya",20,31)
+
+
+student1.display()
+Student2.display()
