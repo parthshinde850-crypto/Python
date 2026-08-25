1d22fb17a2c0461abe9e5ece6e3e4f86a7ca984b
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-25 19:10:45 +0530

    Add files via upload

---
+++ Basic Python/greatestnumberamong2.py
@@ -0,0 +1,11 @@
+a = int(input("Enter first number : "))
+b = int(input("Enter second number : "))
+
+if a > b :
+    print("Greatest number : " , a  )
+elif b > a :
+    print("Greatest number : " , b  )
+else :
+    print("Both numbers are equal ")
+
+
