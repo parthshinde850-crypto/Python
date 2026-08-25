5ee8367426beab5abb12b15bb94d2075c180a3fe
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-25 19:09:22 +0530

    Add files via upload

---
+++ Basic Python/6.py
@@ -0,0 +1,20 @@
+list=[1, 98, "Pineapple", "juice"]
+print("list:", list)
+print("Types of my_list:", type(list))
+
+tuple=(12, 80, "Mango", "Grape")
+print("Tuple:", tuple)
+print("Type of my_tuple:", type(tuple))
+
+set={8, 78, 24, "Jaipur"}
+print("set:", set)
+print("Type of my_set:", type(set))
+
+dict={
+    "name:", "george", 
+    "age:", 15,
+    "city:" , "washington"
+}
+print("Dictionary:", dict)
+print("Type of my_dictionary:",type(dict))
+
