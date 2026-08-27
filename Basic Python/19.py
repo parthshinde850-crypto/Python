4d00afe201da33ae711b97479bcfc3de453a7180
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-27 17:58:55 +0530

    Add files via upload

---
+++ Basic Python/19.py
@@ -0,0 +1,8 @@
+import math
+
+def find_square_root(num):
+    return math.sqrt(num)
+
+# Example usage
+print("Square root of 16 is", find_square_root(16))   # Output: Square root of 16 is 4.0
+print("Square root of 25 is", find_square_root(25))   # Output: Square root of 25 is 5.0
