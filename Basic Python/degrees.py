1f217fe7c35b9af4a5146d538de6de35e154534e
parthshinde850-crypto <parthshinde850@gmail.com>
2026-08-30 16:54:33 +0530

    Add files via upload

---
+++ Basic Python/degrees.py
@@ -0,0 +1,7 @@
+import math
+
+angle_deg = float(input("enter angle in degree:"))
+angle_rad = math.radians(angle_deg)
+print("cosine=", math.cos(angle_rad))
+print("sine=", math.sin(angle_rad))
+print("Tangent=", math.tan(angle_rad))
