ad014d8895cbbc8c7bc24dc0b1d30cfad2391453
parthshinde850-crypto <parthshinde850@gmail.com>
2026-09-16 18:38:48 +0530

    Add files via upload

---
+++ Basic Python/with_statment.py
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(decoration: InputDecoration(labelText: "Email")),
+            TextField(
+              decoration: InputDecoration(labelText: "Password"),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Login"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
